4f4e8232ec3e8245f070d1d48033b5523f3d2ace
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:16:14 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+include "../../premake/common_premake_defines.lua"
+
+project "spirv_cross"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	includedirs {
+		"%{IncludeDir.spirv_cross}",
+		"%{IncludeDir.spirv}/spirv/unified1"
+	}
+
+	files {
+		"./**.h",
+		"./**.c",
+		"./**.hpp",
+		"./**.cpp"
+	}
+
+ 	filter "system:windows"
+		disablewarnings { "4996", "4065" }
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
