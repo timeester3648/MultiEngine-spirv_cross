3c48851b4452f91d33e4d8b59883f780e84b7bbc
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:29 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -20,24 +20,5 @@ project "spirv_cross"
 		"./*.cpp"
 	}
 
- 	filter "system:windows"
+	filter "action:vs*"
 		disablewarnings { "4996", "4065" }
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
